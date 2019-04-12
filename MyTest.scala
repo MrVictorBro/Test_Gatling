c04aa5f1341645c34966738e4f11add4d98e3090
MrVictorBro <49470651+MrVictorBro@users.noreply.github.com>
2019-04-12 15:06:20 +0300

    Update MyTest.scala

--- MyTest.scala
+++ MyTest.scala
@@ -4,6 +4,8 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
+
+
 class MyTest extends Simulation {
 
   val httpProtocol = http
@@ -26,110 +28,62 @@ class MyTest extends Simulation {
 
 // 2. добавление записи
 
-	.exec(http("Load_Page_add_pc")
-		.get("/computers/new")
-		.headers(headers_0))
-	//.pause(7)
-	.exec(http("Add_pc")
+   val csvFeeder = csv("123.csv").random
+
+   val scn = scenario("test")
+	   .feed(csvFeeder)
+       .exec(http("Add_pc")
 		.post("/computers")
-		.headers(headers_0)
-		.formParam("name", "999")
+		.formParam("name", "${name}")
 		.formParam("introduced", "")
 		.formParam("discontinued", "")
-		.formParam("company", ""))  
+		.formParam("company", "")) 
 */
 	
 // 3. удаление записи
-/*	
-	.exec(http("Load_Page_del_pc")
-		.get("/computers/616"))
-		.pause(2)
-	.exec(http("Del_pc")
-		.post("/computers/616/delete"))
-*/		
+	
+  val del = scenario("test")
+    .pause(5)
+    .exec(http("test")
+	  .get("/computers")
+	  .check(regex("""(?<=<td><a href=").*(?=">)""").find.saveAs("computerURL")) 
+	)
+    .exec(http("Del_pc")
+      .post("${computerURL}/delete")
+	)
+		
 //end-----------------------------------------------
 
-
-
-
-
-
-
-// отладка получить ссылку на пк
-  val scn = scenario("test") 
-    .exec(http("test")
-	.get("/computers/")
-	.check(css("a:contains('${searchComputerName}')", "href").saveAs("computerURL"))
-	)
-/*
-  .exec(session => {
-    val response = session("token").as[String]
-    println("------------")
-	//println(s"Response body: \n$response")
-	println("$token")
-	println("------------")
-    session
-  })
-*/
-
-
-
-
-/*
-    .exec(http("Find_link")
-        .get("/computers/")
-        .check(regex("content=\\\"(?<bidkid>[0-9A-Z]*)\\\"").find.saveAs("token"))
-		*/
-	
-	
-
-
-
-//
-  setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))  //запуск одного пользователя
-
-//
-
-
-
-
-
-
-
-//------------------------------
-/*  
-  setUp(scn.inject(
-    rampUsersPerSec(0) to 200 during (5 minutes)
-    )).maxDuration(5 minutes).protocols(httpProtocol)
-*/
-	
 		
-//-------------------------------------------
-/*
+//сценарии
+
+  //setUp(scn.inject(atOnceUsers(20)).protocols(httpProtocol))
+  setUp(del.inject(constantUsersPerSec(1) during (20 seconds)))
+//
+
+//поиск максимума
   setUp(
   scn.inject(
-    rampUsersPerSec(0) to 50 during (5 minutes),			// от 0 до 50
-    constantUsersPerSec(50) during (120 seconds),		// стабильно 50 пользователей
-	rampUsersPerSec(50) to 0 during (20 seconds)		// с 50 до 0
-    )).maxDuration(5 minutes).protocols(httpProtocol)
-*/
+    rampUsersPerSec(0) to 50 during (5 minutes),
+    constantUsersPerSec(50) during (30 minutes), //1	
+    rampUsersPerSec(50) to 100 during (5 minutes),
+    constantUsersPerSec(100) during (30 minutes), //2
+	rampUsersPerSec(100) to 150 during (5 minutes),
+    constantUsersPerSec(150) during (30 minutes), //3
+	rampUsersPerSec(150) to 200 during (5 minutes),
+    constantUsersPerSec(200) during (30 minutes), //4
+	rampUsersPerSec(200) to 0 during (2 minutes),
+    )).maxDuration(90 minutes).protocols(httpProtocol)
+//
 
-
-//------------Справка-----------
-/*
+//тест стабильности
   setUp(
   scn.inject(
-    nothingFor(4 seconds), // 1 Пауза.
-    atOnceUsers(10), // 2 Вводит заданное количество пользователей одновременно.
-    rampUsers(10) during (5 seconds), // 3 Вводит заданное количество пользователей с линейной рамкой в течение заданной продолжительности.
-    constantUsersPerSec(20) during (15 seconds), // 4 Вводит пользователей с постоянной скоростью, определенной в пользователях в секунду, в течение заданного времени. Потребители будут впрыснуты на регулярных интервалах.
-    constantUsersPerSec(20) during (15 seconds) randomized, // 5 Вводит пользователей с постоянной скоростью, определенной в пользователях в секунду, в течение заданного времени. Потребители будут впрыснуты на хаотизированных интервалах.
-    rampUsersPerSec(10) to 20 during (10 minutes), // 6 Вводит пользователей от начальной скорости к целевой скорости, определенной в пользователях в секунду, в течение заданного времени. Потребители будут впрыснуты на регулярных интервалах.
-    rampUsersPerSec(10) to 20 during (10 minutes) randomized, // 7 Вводит пользователей от начальной скорости к целевой скорости, определенной в пользователях в секунду, в течение заданного времени. Потребители будут впрыснуты на хаотизированных интервалах.
-    heavisideUsers(1000) during (20 seconds) // 8 водит заданное число пользователей после гладкого приближения функции шага heaviside, растянутой до заданной продолжительности.
-  ).protocols(httpProtocol)
-)
-*/
- 
+    rampUsersPerSec(0) to 150 during (5 minutes),
+    constantUsersPerSec(150) during (300 minutes), //5h
+    rampUsersPerSec(150) to 0 during (2 minutes),
+    )).maxDuration(310 minutes).protocols(httpProtocol)
+
+
 }
 
